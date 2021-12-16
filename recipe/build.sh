aa51a4cb62d05e34dd1ed24127050e196e29945f
Matthias Diener <mdiener@illinois.edu>
2021-12-16 14:15:55 +0100

    cross fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 # configure tool is mixing CXXFLAGS with CPPFLAGS
 export CFLAGS="${CFLAGS} ${CPPFLAGS}"
 
@@ -10,6 +17,10 @@ export CFLAGS="${CFLAGS} ${CPPFLAGS}"
   --with-z \
   --with-iconv=$PREFIX
 
-make
-make check
+make -j${CPU_COUNT}
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  make check
+fi
+
 make install
